2b7eb94c458563c49b9ad47c1131fe6ccbe24b22
Rudraksh10 <74188117+Rudraksh10@users.noreply.github.com>
2020-11-30 19:11:24 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# bouncy-box
+# bouncyBall
+Bouncy Ball created using matter.js physics engine
---
+++ box.js
@@ -0,0 +1,23 @@
+class Box {
+constructor(x,y,width,height){
+    var box_options ={
+        restitution: 1.0
+    }
+
+    this.body = Bodies.rectangle(x,y,width,height,box_options);
+    this.width = width
+    this.height = height
+    World.add(world,this.body);
+
+}
+display(){
+    var rudraksh = this.body.position
+    var angle = this.body.angle
+    push()
+    translate(rudraksh.x,rudraksh.y)
+    rotate(angle)
+    rectMode(CENTER);
+    rect(0,0,this.width,this.height);
+    pop()
+}
+}
